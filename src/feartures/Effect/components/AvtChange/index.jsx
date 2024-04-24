b2fcb4d8ac59df2bf697e778e7122e759d30ddcc
Tran Trung Kien <coinskus001@gmail.com>
2024-04-25 00:50:28 +0700

    ok

--- src/feartures/Effect/components/AvtChange/index.jsx
+++ src/feartures/Effect/components/AvtChange/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 AvtChange.propTypes = {
@@ -6,10 +6,29 @@ AvtChange.propTypes = {
 };
 
 function AvtChange(props) {
+    const [avt, setAvt] = useState()
+    useEffect(() => {
+        return () => {
+            avt && URL.revokeObjectURL(avt.preview)
+        }
+    }, [avt])
+    const handle = (e) => {
+        const file = e.target.files[0];
+        file.preview = URL.createObjectURL(file)
+        setAvt(file)
+    }
     return (
         <div>
             <h3>Chon File Anh va thay doi hinh nen</h3>
-        </div>
+            <input
+                type='file'
+                onChange={handle}
+            ></input>
+            {avt && (
+                <img src={avt.preview} ></img>
+            )
+            }
+        </div >
     );
 }
 
