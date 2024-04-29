421a1fbc7ce2868fe9b6c4f2c8901debf0cf11f5
Tran Trung Kien <coinskus001@gmail.com>
2024-04-29 11:29:23 +0700

    ok

--- src/feartures/Reduce/components/DemSo/index.jsx
+++ src/feartures/Reduce/components/DemSo/index.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import PropTypes from 'prop-types';
 
 Demso.propTypes = {
 
 };
+const init = 0;
+const Up_action = 'up';
+const Down_action = 'down';
+
+const reducer = (state, action) => {
+    switch (action) {
+        case Up_action: return state + 1;
+        case Down_action: return state - 1;
+        default:
+    }
+}
 
 function Demso(props) {
+    const [count, dispatch] = useReducer(reducer, init);
     return (
         <div>
-            <p>Oci</p>
+            <p style={{ fontSize: 60 }}> {count}</p>
+            <button onClick={() => {
+                dispatch(Up_action);
+            }}>
+                Up
+            </button>
+            <button onClick={() => {
+                dispatch(Down_action);
+            }}>
+                Down
+            </button>
         </div>
     );
 }
--- src/feartures/Reduce/components/TodoList/index.jsx
+++ src/feartures/Reduce/components/TodoList/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 ToDoList.propTypes = {
@@ -6,9 +6,36 @@ ToDoList.propTypes = {
 };
 
 function ToDoList(props) {
+    const [action, setAction] = useState('');
+    const [cv, setCV] = useState([]);
+    const Xuly = () => {
+        setCV(prevCV => [...prevCV, action]);
+        // Clear the input field after adding
+        setAction('');
+    }
+    const Handle = (index) => {
+        setCV(prevCV => prevCV.filter((_, i) => i !== index));
+    }
     return (
         <div>
-            <p>Hello Ae</p>
+            <h2>Ung Dung TodoList don gian</h2>
+            <input
+                type='text'
+                value={action}
+                onChange={e => setAction(e.target.value)}
+            >
+            </input>
+            <button onClick={Xuly}>
+                Add
+            </button>
+            <ul>
+                {cv.map((it, index) => (
+                    <div>
+                        <li key={index}>{it}</li>
+                        <button onClick={() => Handle(index)}>Completed</button>
+                    </div>
+                ))}
+            </ul>
         </div>
     );
 }
