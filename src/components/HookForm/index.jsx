15bef6ceecae331957cf66d3287ea941f9a7882a
Tran Trung Kien <coinskus001@gmail.com>
2024-05-03 00:09:50 +0700

    ok

--- src/components/HookForm/index.jsx
+++ src/components/HookForm/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { TextField, Button } from '@mui/material';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { TextField, Button } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const schema = yup.object().shape({
   username: yup.string().required().min(5),
@@ -10,39 +10,49 @@ const schema = yup.object().shape({
 });
 
 function HookForm() {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm({
-    resolver: yupResolver(schema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
   });
 
   const onSubmit = (data) => {
-    console.log(data); 
+    console.log(data);
     reset(); // Reset form after submission
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField
-        id="username"
-        label="Tài khoản"
-        variant="outlined"
-        {...register("username")}
-        error={!!errors.username}
-        helperText={errors.username?.message}
-      />
-    
-      <br /> 
-      <TextField
-        id="password"
-        label="Mật khẩu"
-        type="password"
-        variant="outlined"
-        {...register("password")}
-        error={!!errors.password}
-        helperText={errors.password?.message}
-      />
-      <br />
-      <Button type="submit" variant="contained">Xác nhận</Button>
-    </form>
+    <div>
+      <h2>Demo React HookForm</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          id="username"
+          label="Tài khoản"
+          variant="outlined"
+          {...register("username")}
+          error={!!errors.username}
+          helperText={errors.username?.message}
+        />
+
+        <br />
+        <TextField
+          id="password"
+          label="Mật khẩu"
+          type="password"
+          variant="outlined"
+          {...register("password")}
+          error={!!errors.password}
+          helperText={errors.password?.message}
+        />
+        <br />
+        <Button type="submit" variant="contained">
+          Xác nhận
+        </Button>
+      </form>
+    </div>
   );
 }
 
